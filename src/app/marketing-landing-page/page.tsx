5f918b162d1076926f637cc91fcdcdd1c952e66b
shikhavaid06-code <shikhavaid06@gmail.com>
2026-08-25 21:08:03 +0530

    Update page.tsx

--- src/app/marketing-landing-page/page.tsx
+++ src/app/marketing-landing-page/page.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import LandingNav from './components/LandingNav';
 import HeroSection from './components/HeroSection';
 import FeaturesSection from './components/FeaturesSection';
+import HowItWorksSection from './components/HowItWorksSection';
 import SocialProofSection from './components/SocialProofSection';
 import WhyCreoSection from './components/WhyCreoSection';
 import PricingSection from './components/PricingSection';
+import FinalCtaSection from './components/FinalCtaSection';
 import FooterSection from './components/FooterSection';
 
 export default function MarketingLandingPage() {
   return (
-    <main className="min-h-screen bg-[#080812] overflow-x-hidden">
+    <main className="min-h-screen bg-creo-bg overflow-x-hidden">
       <LandingNav />
       <HeroSection />
-      <FeaturesSection />
       <SocialProofSection />
+      <FeaturesSection />
+      <HowItWorksSection />
       <WhyCreoSection />
       <PricingSection />
+      <FinalCtaSection />
       <FooterSection />
     </main>
   );
